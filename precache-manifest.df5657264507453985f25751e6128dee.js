dfccc4cb79d7f6db7db670ffca4df270b21ec061
Norbert Janczak <norbert.janczak03@gmail.com>
2019-02-22 23:17:29 +0100

    Updates

---
+++ precache-manifest.df5657264507453985f25751e6128dee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5efb2ccd1f2b7f418809",
+    "url": "/ReactApp/static/js/runtime~main.5efb2ccd.js"
+  },
+  {
+    "revision": "f7b475ad7f0d4aa724d8",
+    "url": "/ReactApp/static/js/main.f7b475ad.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/ReactApp/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "f7b475ad7f0d4aa724d8",
+    "url": "/ReactApp/static/css/main.03e88c14.chunk.css"
+  },
+  {
+    "revision": "72d91c1f44660435a96658fa26572167",
+    "url": "/ReactApp/index.html"
+  }
+];
